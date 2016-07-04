aa8831734a7b9d2e4882c71403ce5ca4e1e0fb6d
Jacqueline Phan & Richie Yi <pair+jtphan14.yirichie@devbootcamp.com>
2016-07-04 13:24:11 -0700

    Drop messages migration table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703053610) do
+ActiveRecord::Schema.define(version: 20160704202308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,12 +26,6 @@ ActiveRecord::Schema.define(version: 20160703053610) do
 
   create_table "layers", force: :cascade do |t|
     t.integer  "track_id",   null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "messages", force: :cascade do |t|
-    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
