c1d8815d63cf4bf1e416dadead7ed1a755766473
Brad Harris <email": null>
2016-07-05 16:50:56 -0700

    works! need to set up when to clear cookies such as when hit record again after login

--- app/assets/javascripts/save.js
+++ app/assets/javascripts/save.js
@@ -3,21 +3,20 @@ function guestSave() {
   $("#save-guest-button").click(function(e) {
     e.preventDefault();
     $(".save-popup").css("display", "block");
-    console.log("Guest tracks var:");
+
+    console.log("In Guest save.. tracks var:");
     console.log(tracks);
+
     Cookies.set("guest_track", tracks, {expires: 1, path: "/"});
 
-    console.log("Cookies:");
+    console.log("In Guest save.. cookies:");
     console.log(Cookies.get('guest_track'));
 
-    // need different code to test if cookies cleared:
-      Cookies.remove("guest_track", {path: "/"});
-
-      if (typeof Cookies.get('guest_track') === "undefined") {
-        console.log("empty");
-      }
-
-      console.log(Cookies.get());
+    // this is a way to test if cookies cleared:
+      // Cookies.remove("guest_track", {path: "/"});
+      // if (typeof Cookies.get('guest_track') === "undefined") {
+      //   console.log("empty");
+      // }
 
   })
 };
@@ -25,19 +24,24 @@ function guestSave() {
 // save
 function saveTrack() {
   $("#save").click(function(e) {
-    console.log("Save clicked, prevent default")
     e.preventDefault();
 
-    // if cookie not empty, i.e. there is a guest_track
-    if (JSON.stringify(Cookies.get()) !== JSON.stringify({})) {
-      // set track = guest_track
-      tracks = Cookies.get('guest_track')
+    console.log("tracks before function. should be undefined b/c haven't recorded anything");
+    console.log(tracks);
+
+    // if cookie not empty, there is a guest_track
+    if (typeof Cookies.get('guest_track') !== "undefined") {
+      // set tracks = guest_track
+      var cookies = Cookies.get('guest_track');
+      var tracks = JSON.parse(cookies);
     }
 
-    // send track back to DB (an array of layer objects).tracks is the recording
-    var name = prompt("Your track is lonely! Give it a name.")
-    console.log("Tracks var:");
+    console.log("tracks after function.. should be same as cookies");
     console.log(tracks);
+
+    // send track back to DB (an array of layer objects).tracks is the recording
+    var name = prompt("Your track is lonely! Give it a name.");
+
     $.ajax({
       url: '/tracks',
       data: {'track': tracks, 'name': name},
