8ce69150e76677e13b92164d8e385aa1bc6fae0b
Jacqueline Phan & Richie Yi <pair+jtphan14.yirichie@devbootcamp.com>
2016-07-07 14:38:25 -0700

    Update track features

--- app/assets/javascripts/save.js
+++ app/assets/javascripts/save.js
@@ -4,20 +4,16 @@ function guestSave() {
   $("#save-guest-button").click(function(e) {
     e.preventDefault();
     $(".save-popup").css("display", "block");
-
-    console.log("In Guest save.. tracks var:");
-    console.log(track);
-
+    // console.log("In Guest save.. tracks var:");
+    // console.log(track);
     Cookies.set("guest_track", track, {expires: 1, path: "/"});
-
-    console.log("In Guest save.. cookies:");
-    console.log(Cookies.get('guest_track'));
+    // console.log("In Guest save.. cookies:");
+    // console.log(Cookies.get('guest_track'));
 
   })
 };
 
 function clearCookies() {
-  console.log("clearing cookies");
   Cookies.remove('guest_track', { path: '/' });
   // after clear Cookies.get('guest_track') returns undefined
 };
@@ -29,8 +25,8 @@ function saveTrack() {
       console.log("i am in save and clicked");
     $(this).css('color', 'gray');
       setTimeout(function() { $('#save').css('color', 'white'); }, 200);
-    console.log("track before function. should be undefined b/c haven't recorded anything");
-    console.log(track);
+    // console.log("track before function. should be undefined b/c haven't recorded anything");
+    // console.log(track);
 
     // if cookie not empty, there is a guest_track. &&..
     // if track is blank, i didn't try recording something else since i logged in.
@@ -40,11 +36,12 @@ function saveTrack() {
       track = JSON.parse(cookies);
     }
 
-    console.log("track after function.. should be same as cookies");
-    console.log(track);
+    // console.log("track after function.. should be same as cookies");
+    // console.log(track);
 
-    // send track back to DB (an array of layer objects).tracks is the recording
     var name = prompt("Your track is lonely! Give it a name.");
+    $("#my-tracks-button").css('color', 'yellow');
+    setTimeout(function() { $("#my-tracks-button").css('color', 'white'); }, 1600);
 
     $.ajax({
       url: '/tracks',
@@ -53,10 +50,11 @@ function saveTrack() {
     })
 
     .done(function(response) {
-      // clear cookies and track []'s after save track
+      // clear cookiherokes and track []'s after save track
       clearCookies();
       track = [];
     })
+
   })
 
 
